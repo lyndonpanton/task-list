9fb1e7fcae384b6ab22eaeac8e065f5fb1a1ac7b
Lyndon Panton <LyndonPanton@gmail.com>
2025-04-23 15:03:54 +0100

    Defined meta information

---
+++ web/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+    <!-- meta -->
+    <meta charset="UTF-8" />
+    <meta name="application-name" content="todo" />
+    <meta name="author" content="Lyndon Mykal Panton" />
+    <meta name="description" content="An application that allows users to add
+            items to a todo list. Todo list items can be modified and deleted." />
+    <meta name="keywords" content="todo, list, app, management" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0,
+            user-scalable=true, minimum-scale=0.25, maximum-scale=4" />
+
+    <!-- title -->
+    <title>Todo List</title>
+
+    <!-- third party -->
+    <!-- Font awesome -->
+    <!-- Google fonts -->
+
+    <!-- first party -->
+    <link rel="stylesheet" type="text/css" href="./css/styles.css" />
+    <script type="text/javascript" src="./js/script.js"></script>
+</head>
+<body>
+    
+</body>
+</html>
